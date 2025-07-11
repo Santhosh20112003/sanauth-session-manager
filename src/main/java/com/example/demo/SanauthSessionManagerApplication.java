e32974610ce5983b62240eb17439cc8720efd8ae
santhosh-celcom <santhoshs@celcomsolutions.com>
2025-07-11 10:51:03 +0530

    first commit

---
+++ src/main/java/com/example/demo/SanauthSessionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanauthSessionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanauthSessionManagerApplication.class, args);
+	}
+
+}
